2cd873355cf3111888384288246a2b99b41ed76b
lydiah1wachira <lydiahcedric@gmail.com>
2022-05-15 23:50:27 +0300

    create a class to create quote objects.

---
+++ app/models.py
@@ -0,0 +1,11 @@
+
+
+
+class Quote:
+  '''
+  Blueprint class to display random quotes from the API
+  '''
+  
+  def __init__(self, author, quote):
+    self.author = author
+    self.quote = quote
