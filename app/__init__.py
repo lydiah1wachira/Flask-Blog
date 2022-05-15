c422146f37af5bc9777e1fa701e22e09666ca99c
lydiah1wachira <lydiahcedric@gmail.com>
2022-05-16 00:16:54 +0300

    import and initialize bootstrap.'

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
-
+from flask_bootstrap import Bootstrap
 from config import config_options
+
+bootstrap = Bootstrap()
 
 
 
@@ -8,6 +10,13 @@ def create_app(config_name):
 
   app=Flask(__name__)
 
+  #creating the app configurations
   app.config.from_object(config_options[config_name])
+
+   #Initializing flask extensions
+  bootstrap.init_app(app)
+
+  
+
 
   return app
