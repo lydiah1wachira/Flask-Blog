ec4eec61e8aa9e593a8d503a76aafff93f4b466f
lydiah1wachira <lydiahcedric@gmail.com>
2022-05-16 15:26:30 +0300

    add initial migration and upgrade to new migration.

--- manage.py
+++ manage.py
@@ -1,7 +1,9 @@
-from imghdr import tests
-from app import create_app
 
+from app import create_app,db
+from app.models import User
 from flask_script import Manager,Server
+from  flask_migrate import Migrate, MigrateCommand
+
 
 app=create_app('development')
 
@@ -13,8 +15,15 @@ def test():
     Run the unit tests 
     '''
     import unittest
-    test = unittest.TestLoader().discover('tests')
+    tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
