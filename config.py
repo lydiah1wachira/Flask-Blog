77743fc77dd5307109ee67423876e19815ff6866
lydiah1wachira <lydiahcedric@gmail.com>
2022-05-16 03:12:52 +0300

    set up and initialized the database and SQLAlchemy.

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class Config:
     '''
     General configuration parent class
     '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wachira:Lydiah007@localhost/blogs'
   
     
     @staticmethod
